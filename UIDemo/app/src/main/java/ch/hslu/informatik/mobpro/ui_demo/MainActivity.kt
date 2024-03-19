0df379901396f8eab8ecf6e850b8305f52fc93a9
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-03-19 09:17:21 +0100

    added style and more

--- UIDemo/app/src/main/java/ch/hslu/informatik/mobpro/ui_demo/MainActivity.kt
+++ UIDemo/app/src/main/java/ch/hslu/informatik/mobpro/ui_demo/MainActivity.kt
@@ -4,8 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.activity_main_fragment_container, MainFragment.newInstance())
+                .commit()
+        }
     }
 }
---
+++ UIDemo/app/src/main/java/ch/hslu/informatik/mobpro/ui_demo/MainFragment.kt
@@ -0,0 +1,33 @@
+package ch.hslu.informatik.mobpro.ui_demo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance(): MainFragment {
+            return MainFragment()
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.Button).setOnClickListener {
+            // Handle button click
+        }
+    }
+}
