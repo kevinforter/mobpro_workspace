e20298d6798dd6f51b01200bfde32d8c10e0cb48
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-04-13 20:02:39 +0200

    added MyMapActivity and layout

---
+++ Intent_and_widget/app/src/main/java/ch/hslu/informatik/mobpro/intent_and_widget/MyMapActivity.kt
@@ -0,0 +1,18 @@
+package ch.hslu.informatik.mobpro.intent_and_widget
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import ch.hslu.informatik.mobpro.intent_and_widget.R
+
+class MyMapActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_map)
+
+        val textView = findViewById<TextView>(R.id.textViewCoordinates)
+        textView.text = "Location: ${intent.data}"
+    }
+}
