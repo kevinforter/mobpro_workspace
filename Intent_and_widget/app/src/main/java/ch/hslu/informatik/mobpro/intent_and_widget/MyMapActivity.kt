ebff665714e74d9d9f2eda0ea241cf3ab5f65e93
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-04-14 02:33:22 +0200

    changed to binding gem. Vorlesung

--- Intent_and_widget/app/src/main/java/ch/hslu/informatik/mobpro/intent_and_widget/MyMapActivity.kt
+++ Intent_and_widget/app/src/main/java/ch/hslu/informatik/mobpro/intent_and_widget/MyMapActivity.kt
@@ -1,18 +1,19 @@
 package ch.hslu.informatik.mobpro.intent_and_widget
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import ch.hslu.informatik.mobpro.intent_and_widget.R
+import ch.hslu.informatik.mobpro.intent_and_widget.databinding.ActivityMyMapBinding
 
 class MyMapActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId", "SetTextI18n")
+    private lateinit var binding: ActivityMyMapBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_map)
-
-        val textView = findViewById<TextView>(R.id.textViewCoordinates)
-        textView.text = "Location: ${intent.data}"
+        binding = ActivityMyMapBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.longLat.text = intent.data?.schemeSpecificPart
     }
 }
