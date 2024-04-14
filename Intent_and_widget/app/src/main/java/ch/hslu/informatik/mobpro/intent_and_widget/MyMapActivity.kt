ba57dbf9fbf09dc3f6487cf9e9835099e664e8db
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-04-14 02:40:06 +0200

    deleted unused imports

--- Intent_and_widget/app/src/main/java/ch/hslu/informatik/mobpro/intent_and_widget/MyMapActivity.kt
+++ Intent_and_widget/app/src/main/java/ch/hslu/informatik/mobpro/intent_and_widget/MyMapActivity.kt
@@ -1,11 +1,7 @@
 package ch.hslu.informatik.mobpro.intent_and_widget
 
-import android.annotation.SuppressLint
-import android.app.Activity
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import ch.hslu.informatik.mobpro.intent_and_widget.R
 import ch.hslu.informatik.mobpro.intent_and_widget.databinding.ActivityMyMapBinding
 
 class MyMapActivity : AppCompatActivity() {
